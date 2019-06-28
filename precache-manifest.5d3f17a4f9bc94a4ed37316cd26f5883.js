e30dcbddeb16c885e5181a06a17617a891162fb5
test <test>
2019-06-28 14:45:47 -0400

    Updates

---
+++ precache-manifest.5d3f17a4f9bc94a4ed37316cd26f5883.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d992e67b84095fee4a38d331e505679",
+    "url": "/react-router-basics/index.html"
+  },
+  {
+    "revision": "1dd341733f81d474e8c0",
+    "url": "/react-router-basics/static/css/main.6c6ef733.chunk.css"
+  },
+  {
+    "revision": "71794c7b3b8060471bbf",
+    "url": "/react-router-basics/static/js/2.bfcbba94.chunk.js"
+  },
+  {
+    "revision": "1dd341733f81d474e8c0",
+    "url": "/react-router-basics/static/js/main.06a52332.chunk.js"
+  },
+  {
+    "revision": "f63fa6d8227b4a7b6af4",
+    "url": "/react-router-basics/static/js/runtime~main.ddbe1d99.js"
+  }
+]);
